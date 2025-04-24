cafad29e10e3b74a3cbc6ff4df66bc340211294b
Diego Viana <dsantos@inf.ufsm.br>
2025-04-23 21:24:07 -0300

    add and config prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,9 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
 import { env } from './env'
+import { mainRoutes } from './routes/main'
+import { authRoutes } from './routes/auth'
+import { adminRoutes } from './routes/admin'
 
 const server = express()
 server.use(cors())
@@ -9,9 +12,9 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.static('public'))
 
-server.get('/apicls/ping', (req, res) => {
-  res.json({ pong: true })
-})
+server.use('/api/auth', authRoutes)
+server.use('/api/admin', adminRoutes)
+server.use('/api', mainRoutes)
 
 server.listen({ port: env.PORT }, () => {
   console.log(`🚀 HTTP Server Running! http://localhost:${env.PORT}`)
