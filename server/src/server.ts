5a44fb946a07cc60812955c0931cbf0b18d56992
Diego Viana <dsantos@inf.ufsm.br>
2025-04-23 21:03:01 -0300

    changing fastify to express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,42 +1,18 @@
-import { fastifyCors } from '@fastify/cors'
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import fastify from 'fastify'
-import {
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-  ZodTypeProvider,
-} from 'fastify-type-provider-zod'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
 import { env } from './env'
-import { pingRoute } from './routes/ping-route'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const server = express()
+server.use(cors())
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(express.static('public'))
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
-
-app.register(fastifyCors, {
-  origin: true,
+server.get('/apicls/ping', (req, res) => {
+  res.json({ pong: true })
 })
 
-app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'Blog',
-      version: '0.0.1',
-      description: 'API for Blog',
-    },
-  },
-  transform: jsonSchemaTransform,
-})
-
-app.register(fastifySwaggerUi, {
-  routePrefix: '/docs',
-})
-
-app.register(pingRoute)
-
-app.listen({ port: env.PORT }).then(() => {
+server.listen({ port: env.PORT }, () => {
   console.log(`🚀 HTTP Server Running! http://localhost:${env.PORT}`)
 })
